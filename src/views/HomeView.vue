c74ec7672a4016a38591283a94e00df9f9cf925c
MedAGI2025 <940948244@qq.com>
2025-05-12 11:38:26 +0100

    update keynote

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,7 +60,8 @@
       <div class="main-title-deco"></div>
       <div class="description">
         Medical image analysis has traditionally relied on AI models trained on specific datasets, which often becomes challenging when transferred to data from different medical centers. This inherent limitation has inspired a growing interest in general medical AI, capable of seamlessly adapting to various medical scenarios, data modalities, and task formulations prevalent across hospitals and institutions. Drawing parallels from the computer vision and natural language processing domains, foundation models, such as large language and vision-language models like GPT, LLaMA, Gemini, stand out as quintessential general AI solutions. These models have demonstrated remarkable proficiency in a myriad of tasks owing to their massive training datasets and substantial model sizes. Yet, the translation of these successes to medical, namely the general medical AI, remains nascent. This workshop is designed to serve as a confluence of insights from the current landscape of medical AI and foundation models. It aims to foster discussions that will pave the way for the evolution of task-specific medical AI systems into more generalized frameworks capable of tackling a diverse range of tasks, datasets, and domains. In doing so, it aspires to act as a catalyst in synergizing the medical domain with cutting-edge advancements in machine learning, particularly in the realm of large-scale foundation models.
-    </div>
+        
+      </div>
     </div>
     <!-- End Workshop Description -->
 
--- src/views/KeynoteView.vue
+++ src/views/KeynoteView.vue
@@ -8,7 +8,81 @@
     <!-- Main Title End -->
 
     <!-- Preliminary Keynotes -->
-		<h2>TBD</h2>
+		<div v-if="currentYear === '2025'">
+      <div id="keynotes">
+			<!-- <h2>Keynotes</h2><hr> -->
+			<div id="keynote-container">
+        <div class="keynote-box">
+					<div class="keynote-title">AI-driven Drug Discovery in the LLM Era</div>
+          <div class="profile-box">
+            <img src="@/assets/keynotes/Prof_Jaewoo_Kang.jpg" class="profile" style="object-position: top;">
+            <div class="">
+              <div class="keynote-speaker">Prof. Jaewoo Kang</div>
+              <div class="affiliation"><i>Korea University</i></div>
+            </div>
+          </div>
+          <div class="speaker-bio">
+            <b>Abstract.</b> Large Language Models (LLMs) are emerging as transformative tools across various industries, with significant implications for healthcare and pharmaceuticals. The pharmaceutical sector, characterized by its reliance on extensive knowledge, stands to gain substantial benefits from LLMs. This presentation will delve into how LLMs can enhance productivity in drug discovery by enabling comprehensive analysis of scientific papers and patents, facilitating the generation of novel scientific hypotheses, design of novel drugs, accurate prediction of the pharmacological properties of the designed drugs, and streamlining the drafting of regulatory approval documents. Building upon the speaker's expertise in AI-driven precision medicine and drug discovery, and recent advancements in biomedically specialized LLMs, this talk will provide insights into the current state and future directions of LLMs in drug discovery.
+            <br><b>Bio.</b> Dr. Jaewoo Kang is a Professor of Computer Science at Korea University (since 2006) and CEO of AIGEN Sciences (founded in 2021), where he leads AI-driven precision medicine and drug discovery efforts. His expertise is in biomedical AI, with a focus on advancing AI solutions for healthcare. He holds a Ph.D. in Computer Science from the University of Wisconsin-Madison (2003) and was previously an Assistant Professor at North Carolina State University (2003-2006). Dr. Kang has made significant contributions to biomedical AI, notably the development of BioBERT (over 7,000 citations). His research team has achieved remarkable success in global precision medicine and drug discovery challenges, securing seven wins in prestigious competitions including Dream Challenges, BioASQ, and BioCreative over the past eight years. At AIGEN Sciences, he is focused on integrating AI technologies with biomedical research to advance drug discovery and personalized medicine.
+          </div>
+				</div>
+				<div class="keynote-box">
+					<!-- <div class="keynote-title">Pre-Foundation-Model-Era for Medical Imaging Analysis: Studies on Self-supervised Learning</div> -->
+          <div class="profile-box">
+            <img src="@/assets/keynotes/Jin_Ke.jpg" class="profile">
+            <div>
+              <div class="keynote-speaker">Dr Jin Ke</div>
+              <div class="affiliation"><i>Shanghai Jiao Tong University, Shanghai, China</i></div>
+            </div>
+          </div>
+          <div class="speaker-bio">
+            Jing Ke is a tenured associate professor at Shanghai Jiao Tong University and holds the academic title of Adjunct Senior Lecturer at the University of New South Wales. Her primary research focus is computer-assisted pathology image analysis, with more than fifty published papers in top journals and conferences, including IEEE Transactions on Medical Imaging (TMI), Medical Image Analysis (MedIA), and MICCAI. Also, Jing has extensive commercial experience in GPU technology, having worked with leading companies like AMD. She has also published over ten papers on computer architectures and parallel computing in renowned venues such as MICRO, DAC, and IPDPS., and authored a book on GPU architecture analysis.
+          </div>
+				</div>
+			</div>
+    </div>
+  </div>
+  <div v-else>
+    <div id="keynotes">
+			<!-- <h2>Keynotes</h2><hr> -->
+      <div class="keynote-box">
+        <div class="keynote-title">Towards Human Centered AI: Large Language Models and Domain Knowledge</div>
+      <div class="profile-box">
+        <img src="@/assets/keynotes/dimitris.jpg" class="profile" style="object-position: top;">
+        <div class="">
+          <div class="keynote-speaker">
+            <a href="https://people.cs.rutgers.edu/~dnm/bio.html" target="_blank">Prof. Dimitris Metaxas</a>
+          </div>
+          <div class="affiliation"><i>Department of Computer Science, Rutgers The State University of New Jersey</i></div>
+        </div>
+      </div>
+      <div class="speaker-bio">
+        <b>Bio.</b> Dr. Dimitris Metaxas is a Distinguished Professor in the Department of Computer Science at Rutgers University since July 2007. From September 2001 to June 2007 he was a Professor in the same department. He is currently directing the Center for Computational Biomedicine, Imaging and Modeling (CBIM). From January 1998 to September 2001 he was a tenured Associate Professor in the Computer and Information Science Department of the University of Pennsylvania and Director of the VAST Lab. Prior to this he was an Assistant Professor in the same department since 1992. Prof. Metaxas received a Diploma in Electrical Engineering from the National Technical University of Athens of Athens Greece in 1986, an M.Sc. in Computer Science from the University of Maryland, College Park in 1988, and a Ph.D. in Computer Science from the University of Toronto, Ontario, Canada in 1992.
+      </div>
+      </div>
+			<div id="keynote-container">
+				<div class="keynote-box">
+          <div class="keynote-title">Foundation Models for Medical Image Analysis – Preparing for the Next “iPhone Moment” in MICCAI</div>
+          <div class="profile-box">
+            <img src="@/assets/keynotes/mathias.jpg" class="profile">
+            <div>
+              <div class="keynote-speaker">
+                <a href="https://arcade.cs.jhu.edu/members/mathias-unberath.html" target="_blank">Prof. Mathias Unberath</a>
+              </div>
+              <div class="affiliation"><i>Department of Computer Science, Johns Hopkins University</i></div>
+            </div>
+          </div>
+          <div class="speaker-bio">
+            <b>Keynote Slide: </b>[<a :href="require('@/assets/keynotes/mathias_slide.pdf').default" target="_blank" rel="noopener noreferrer">slide (PDF)</a>]<br>
+            <!-- <b>Keynote Slide:</b><a href="@/assets/papers_2024/P1.pdf" target="_blank" class="slides-button"> [slides]</a><br> -->
+            <b>Abstract.</b> A new class of machine learning models, often termed foundation models, has started to receive widespread attention in -- and well beyond MICCAI. Trained in flexible manner on large and diverse datasets, foundation models offer unprecedented generalization ability, and if leveraged correctly, broad utility. In this talk, I will first paint a picture of how foundation models may set us off on a trajectory towards the next iPhone moment in MICCAI. I will then substantiate this by providing recent examples of how we develop and use foundation models to tame perpetually challenging problems across MICCAI, concluding with an elaborate discussion of open challenges and opportunities.
+            <br><b>Bio.</b> Mathias Unberath is the John C. Malone Associate Professor of Computer Science at Johns Hopkins University, core faculty in the Malone Center for Engineering in Healthcare and the Laboratory for Computational Sensing and Robotics, and affiliate faculty in the Institute for Assured Autonomy. He holds secondary appointments in the School of Medicine. With his team, the ARCADE research group on Advanced Robotics and Computationally AugmenteD Environments, he builds the future of computer-assisted medicine. Through synergistic research on imaging, computer vision, machine learning, and interaction design, they create human-centered solutions that are embodied in emerging technology such as mixed reality and robotics. Mathias has published more than 175 journal and conference articles, and has received numerous awards, grants, and fellowships, including the NSF Career, NIH NIBIB R21 Trailblazer and Google Research Scholar Award, and more than 20 international paper awards.
+          </div>
+				</div>
+
+			</div>
+    </div>
+    </div>
     <!-- Preliminary Keynotes End -->
   </div>
 </template>
